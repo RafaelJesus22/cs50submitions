06066499f7701a8109c3629b22601355d60d58ab
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2020-08-01 15:10:14 -0300

    pronlem set 4, filter(less)

--- week2/problemSet2/substitution.c
+++ week2/problemSet2/substitution.c
@@ -1,118 +1,118 @@
-#include <cs50.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdio.h>
+// #include <cs50.h>
+// #include <string.h>
+// #include <ctype.h>
+// #include <stdio.h>
 
-/*
-* invalid keys
-* no key at all "usage"
-* key with length less  than 26 characters "key must be..."
-* key with no alphabetic characters "key must only contain alphabetic characters"
-* key with repeted characters
-*/
+// /*
+// * invalid keys
+// * no key at all "usage"
+// * key with length less  than 26 characters "key must be..."
+// * key with no alphabetic characters "key must only contain alphabetic characters"
+// * key with repeted characters
+// */
 
-/*
-TODO
-[X] - get the key
-[X] - validate the key
-    [X] - no key
-    [X] - key with length different than 26
-    [X] - key with no alphabetic characters
-    [X] - key with repeted characters
-[X] - get plaintext
-[X] - encipher
-    [X] - change letters and keep non alphabetic characters
-    [X] - keep plaintext case
-[X] - print ciphertext
-*/
+// /*
+// TODO
+// [X] - get the key
+// [X] - validate the key
+//     [X] - no key
+//     [X] - key with length different than 26
+//     [X] - key with no alphabetic characters
+//     [X] - key with repeted characters
+// [X] - get plaintext
+// [X] - encipher
+//     [X] - change letters and keep non alphabetic characters
+//     [X] - keep plaintext case
+// [X] - print ciphertext
+// */
 
-void EncipherAndShow(string key);
+// void EncipherAndShow(string key);
 
-const int SIZE = 26;
+// const int SIZE = 26;
 
-int main(int argc, string argv[])
-{
-    string key = argv[1];
+// int main(int argc, string argv[])
+// {
+//     string key = argv[1];
 
-    // in case user input no key
-    if (!key)
-    {
-        puts("Usage: ./substitution key");
-        return 1;
-    }
-    else
-    {
-        // validates key size
-        if (strlen(key) < SIZE || strlen(key) > SIZE)
-        {
-            puts("Key must contain 26 characters.");
-            return 1;
-        }
+//     // in case user input no key
+//     if (!key)
+//     {
+//         puts("Usage: ./substitution key");
+//         return 1;
+//     }
+//     else
+//     {
+//         // validates key size
+//         if (strlen(key) < SIZE || strlen(key) > SIZE)
+//         {
+//             puts("Key must contain 26 characters.");
+//             return 1;
+//         }
 
-        // validates if the key is only alphabetic
-        for (int i = 0; i < SIZE; i++)
-        {
-            if (!isalpha(key[i]))
-            {
-                puts("key must only contain alphabetic characters.");
-                return 1;
-            }
-        }
+//         // validates if the key is only alphabetic
+//         for (int i = 0; i < SIZE; i++)
+//         {
+//             if (!isalpha(key[i]))
+//             {
+//                 puts("key must only contain alphabetic characters.");
+//                 return 1;
+//             }
+//         }
 
-        // validades repeted characters
-        for (int i = 0; i < SIZE; i++)
-        {
-            for (int j = 0; j < SIZE; j++)
-            {
-                if (key[j] == key[i] && i != j)
-                {
-                    puts("Key must not contain repeted characters.");
-                    return 1;
-                }
-            }
-        }
-    }
+//         // validades repeted characters
+//         for (int i = 0; i < SIZE; i++)
+//         {
+//             for (int j = 0; j < SIZE; j++)
+//             {
+//                 if (key[j] == key[i] && i != j)
+//                 {
+//                     puts("Key must not contain repeted characters.");
+//                     return 1;
+//                 }
+//             }
+//         }
+//     }
 
-    EncipherAndShow(key);
+//     EncipherAndShow(key);
 
-}
+// }
 
-void EncipherAndShow(string key)
-{
-    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string plaintext = get_string("plaintext: ");
-    string cipheredText = plaintext;
+// void EncipherAndShow(string key)
+// {
+//     string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     string plaintext = get_string("plaintext: ");
+//     string cipheredText = plaintext;
 
-    // loops through the plaintext
-    for (int i = 0; i < SIZE; i++)
-    {
-        // validates non alphabetic caracters
-        if (isalpha(plaintext[i]))
-        {
-            // for every character in alpha
-            for (int j = 0; j < SIZE; j++)
-            {
-                // if current plaintext letter is equal to alpha[current j]
-                if (alpha[j] == toupper(plaintext[i]))
-                {
-                    if (isupper(plaintext[i]))
-                    {
-                        cipheredText[i] = toupper(key[j]);
-                        break;
-                    }
+//     // loops through the plaintext
+//     for (int i = 0; i < SIZE; i++)
+//     {
+//         // validates non alphabetic caracters
+//         if (isalpha(plaintext[i]))
+//         {
+//             // for every character in alpha
+//             for (int j = 0; j < SIZE; j++)
+//             {
+//                 // if current plaintext letter is equal to alpha[current j]
+//                 if (alpha[j] == toupper(plaintext[i]))
+//                 {
+//                     if (isupper(plaintext[i]))
+//                     {
+//                         cipheredText[i] = toupper(key[j]);
+//                         break;
+//                     }
 
-                    if (islower(plaintext[i]))
-                    {
-                        cipheredText[i] = tolower(key[j]);
-                        break;
-                    }
-                }
-            }
-        }
-    }
+//                     if (islower(plaintext[i]))
+//                     {
+//                         cipheredText[i] = tolower(key[j]);
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//     }
 
 
-    //print cipheredtext
-    printf("ciphertext: %s\n", cipheredText);
+//     //print cipheredtext
+//     printf("ciphertext: %s\n", cipheredText);
 
-}
+// }
--- week3/problemSet3/runoff.c
+++ week3/problemSet3/runoff.c
@@ -1,202 +1,202 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
+// #include <cs50.h>
+// #include <stdio.h>
+// #include <string.h>
 
-// declare max voters and max candidates
-#define MAX_CANDIDATES 9
-#define MAX_VOTERS 9
-
-
-// defines type candidate
-typedef struct
-{
-    string name;
-    int votes;
-    bool eliminated;
-}
-candidate;
-
-// create array of candidates and array of preferences
-candidate candidates[MAX_CANDIDATES];
-int preferences[MAX_VOTERS][MAX_CANDIDATES];
-int candidate_count;
-int voter_count;
+// // declare max voters and max candidates
+// #define MAX_CANDIDATES 9
+// #define MAX_VOTERS 9
 
 
-// function prototypes
-bool vote(int voter, int rank, string name);
-void tabulate(void);
-bool print_winner(void);
-int find_min(void);
-bool is_tie(int min);
-void eliminate(int min);
+// // defines type candidate
+// typedef struct
+// {
+//     string name;
+//     int votes;
+//     bool eliminated;
+// }
+// candidate;
+
+// // create array of candidates and array of preferences
+// candidate candidates[MAX_CANDIDATES];
+// int preferences[MAX_VOTERS][MAX_CANDIDATES];
+// int candidate_count;
+// int voter_count;
 
 
-int main(int argc, string argv[])
-{
-    // checks if any candidate was passed
-    if (argc < 2)
-    {
-        printf("Usage: plurality [candidate ...]\n");
-        return 1;
-    }
+// // function prototypes
+// bool vote(int voter, int rank, string name);
+// void tabulate(void);
+// bool print_winner(void);
+// int find_min(void);
+// bool is_tie(int min);
+// void eliminate(int min);
 
-    candidate_count = argc - 1; // define number of candidates
+
+// int main(int argc, string argv[])
+// {
+//     // checks if any candidate was passed
+//     if (argc < 2)
+//     {
+//         printf("Usage: plurality [candidate ...]\n");
+//         return 1;
+//     }
+
+//     candidate_count = argc - 1; // define number of candidates
     
-    // checks if number of candidates is bigger than allowed
-    if (candidate_count > MAX_CANDIDATES)
-    {
-        printf("Maximum number of candidates is %i\n", MAX_CANDIDATES);
-        return 2;
-    }
+//     // checks if number of candidates is bigger than allowed
+//     if (candidate_count > MAX_CANDIDATES)
+//     {
+//         printf("Maximum number of candidates is %i\n", MAX_CANDIDATES);
+//         return 2;
+//     }
 
-    voter_count = get_int("Number of voters: ");
+//     voter_count = get_int("Number of voters: ");
 
-    if (voter_count > MAX_VOTERS)
-    {
-        printf("Maximum number of voters is %i\n", MAX_VOTERS);
-        return 3;
-    }
-    // initialize candidates array
-    for (int i = 0; i < candidate_count; i++)
-    {
-        candidates[i].name = argv[i + 1];
-        candidates[i].votes = 0;
-        candidates[i].eliminated = false;
-    }
+//     if (voter_count > MAX_VOTERS)
+//     {
+//         printf("Maximum number of voters is %i\n", MAX_VOTERS);
+//         return 3;
+//     }
+//     // initialize candidates array
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         candidates[i].name = argv[i + 1];
+//         candidates[i].votes = 0;
+//         candidates[i].eliminated = false;
+//     }
 
-    // gets the rank for each voter
-    for (int i = 0; i < voter_count; i++)
-    {
-       for (int j = 0; j < candidate_count; j++)
-       {
-            while (true)
-            {
-                printf("Rank %i: ", j+1);
-                string name = get_string("");
+//     // gets the rank for each voter
+//     for (int i = 0; i < voter_count; i++)
+//     {
+//        for (int j = 0; j < candidate_count; j++)
+//        {
+//             while (true)
+//             {
+//                 printf("Rank %i: ", j+1);
+//                 string name = get_string("");
 
-                if (vote(i, j, name))
-                {
-                    break;
-                }
-            }
-        }
+//                 if (vote(i, j, name))
+//                 {
+//                     break;
+//                 }
+//             }
+//         }
 
-        printf("\n");
-    }
+//         printf("\n");
+//     }
 
-    while (!print_winner())
-    {
-        if (!is_tie(find_min()))
-        {
-            eliminate(find_min());
-        }
-    }
+//     while (!print_winner())
+//     {
+//         if (!is_tie(find_min()))
+//         {
+//             eliminate(find_min());
+//         }
+//     }
 
-}
-
-
-bool vote(int voter, int rank, string name)
-{
-    // look for a candidate called name
-    for (int i = 0; i < candidate_count; i++)
-    {
-        // if candidate found, update preferences so that they are the voter's rank preferences and return true
-        if (strcmp(candidates[i].name, name) == 0)
-        {
-            preferences[voter][rank] = i;
-            return true;
-        }
-    }
+// }
 
 
-    // if no candidate found, don't update any preferences at all and return false
-    return false;
-}
+// bool vote(int voter, int rank, string name)
+// {
+//     // look for a candidate called name
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         // if candidate found, update preferences so that they are the voter's rank preferences and return true
+//         if (strcmp(candidates[i].name, name) == 0)
+//         {
+//             preferences[voter][rank] = i;
+//             return true;
+//         }
+//     }
 
 
-void tabulate(void)
-{
-    // for voter in preferences
-    for (int i = 0; i < voter_count; i++)
-    {
-        // for rank of voter preferences
-        for (int j = 0; j < candidate_count; j++)
-        {
-            int vote = preferences[i][j]; // handles the candidate position
-            if (!candidates[vote].eliminated) // if candidate is not eliminated
-            {
-                candidates[vote].votes++;
-                break;
-            }
-        }
-    }
-}
+//     // if no candidate found, don't update any preferences at all and return false
+//     return false;
+// }
 
 
-bool print_winner(void)
-{
-    tabulate();
-
-    int min_votes = voter_count / 2 + 1;
-    int winner_count = 0;
-
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].votes == min_votes && !candidates[i].eliminated)
-        {
-            printf("%s\n", candidates[i].name);
-            winner_count++;
-        }
-    }
-
-    if (winner_count > 0)
-    {
-        return true;
-    }
-
-    return false;
-}
+// void tabulate(void)
+// {
+//     // for voter in preferences
+//     for (int i = 0; i < voter_count; i++)
+//     {
+//         // for rank of voter preferences
+//         for (int j = 0; j < candidate_count; j++)
+//         {
+//             int vote = preferences[i][j]; // handles the candidate position
+//             if (!candidates[vote].eliminated) // if candidate is not eliminated
+//             {
+//                 candidates[vote].votes++;
+//                 break;
+//             }
+//         }
+//     }
+// }
 
 
-int find_min(void)
-{
-    int min = candidates[0].votes;
+// bool print_winner(void)
+// {
+//     tabulate();
 
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (min > candidates[i].votes)
-        {
-            min = candidates[i].votes;
-        }
-    }
+//     int min_votes = voter_count / 2 + 1;
+//     int winner_count = 0;
 
-    // return the minimun number of voter of anyone still in the election
-    return min;
-}
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         if (candidates[i].votes == min_votes && !candidates[i].eliminated)
+//         {
+//             printf("%s\n", candidates[i].name);
+//             winner_count++;
+//         }
+//     }
 
+//     if (winner_count > 0)
+//     {
+//         return true;
+//     }
 
-bool is_tie(int min)
-{
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].votes != min && !candidates[i].eliminated)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+//     return false;
+// }
 
 
-void eliminate(int min)
-{
-    // eliminate anyone still in the race who has the min number of votes
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (candidates[i].votes == min && candidates[i].eliminated == false)
-        {
-            candidates[i].eliminated = true;
-        }
-    }
-}
+// int find_min(void)
+// {
+//     int min = candidates[0].votes;
+
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         if (min > candidates[i].votes)
+//         {
+//             min = candidates[i].votes;
+//         }
+//     }
+
+//     // return the minimun number of voter of anyone still in the election
+//     return min;
+// }
+
+
+// bool is_tie(int min)
+// {
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         if (candidates[i].votes != min && !candidates[i].eliminated)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+// void eliminate(int min)
+// {
+//     // eliminate anyone still in the race who has the min number of votes
+//     for (int i = 0; i < candidate_count; i++)
+//     {
+//         if (candidates[i].votes == min && candidates[i].eliminated == false)
+//         {
+//             candidates[i].eliminated = true;
+//         }
+//     }
+// }
